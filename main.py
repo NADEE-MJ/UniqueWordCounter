73f46468f166dbb767b8a8cb698f04880c2c2637
NADEE-MJ <nadeem.maida@gmail.com>
2022-01-25 18:38:28 -0800

    added code to run class in main, and did a test run

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-url = "https://shakespeare.folger.edu/downloads/txt/romeo-and-juliet_TXT_FolgerShakespeare.txt"
+import uniqueWordCounter as c
+
+def main():
+    url = "https://shakespeare.folger.edu/downloads/txt/romeo-and-juliet_TXT_FolgerShakespeare.txt"
+    rAndJ = c.UniqueWordCounter(url)
+    rAndJ.getUniqueWords()
+    rAndJ.printNumberOfUniqueWords()
+    rAndJ.printTopTenWords()
+    rAndJ.saveFormattedText("test run\\formattedText.txt")
+    rAndJ.saveUniqueWords("test run\\uniqueWords.json")
+
+if __name__ == "__main__":
+    main()
--- uniqueWordCounter.py
+++ uniqueWordCounter.py
@@ -8,44 +8,44 @@ class UniqueWordCounter():
     """
     def __init__(self, url):
         self.url = url
-        self.getformattedText()
-        self.getEntireTextSplit()
+        self.__getformattedText()
+        self.__getEntireTextSplit()
 
-    def getURLData(self):
+    def __getURLData(self):
         return requests.get(self.url)
 
-    def getformattedText(self):
-        urlResponse = self.getURLData()
+    def __getformattedText(self):
+        urlResponse = self.__getURLData()
         punctuationToRemove = '[' + string.punctuation.replace("'", "") + ']'
         self.formattedText = re.sub(punctuationToRemove, ' ', urlResponse.text)
 
-    def getEntireTextSplit(self):
+    def __getEntireTextSplit(self):
         self.EntireTextSplit = self.formattedText.lower().split()
 
     def getUniqueWords(self):
         self.uniqueWords = Counter(self.EntireTextSplit)
 
     def printNumberOfUniqueWords(self):
-        print("There are %d number of words in the provided text!" % (len(self.uniqueWords))) 
+        print("There are %d words in the provided text!" % (len(self.uniqueWords))) 
 
     def printTopTenWords(self):
         topTen = self.uniqueWords.most_common(10)
         print("Word:  # of Occurances")
         for word in topTen:
-            print("%s:  " % (word[0], word[1]))
+            print("%s:  %d" % (word[0], word[1]))
 
     def saveFormattedText(self, path):
         f = open(path, 'w')
         f.write(self.formattedText)
         f.close()
 
-    def getSortedUniqueWords(self):
+    def __getSortedUniqueWords(self):
         sortedUniqueWords = {}
         for word in sorted(self.uniqueWords):
-            sortedUniqueWords[word] = sortedUniqueWords[word]
+            sortedUniqueWords[word] = self.uniqueWords[word]
         return sortedUniqueWords
 
     def saveUniqueWords(self, path):
         f = open(path, 'w')
-        json.dump(self.getSortedUniqueWords(), f, indent = 4)
+        json.dump(self.__getSortedUniqueWords(), f, indent = 4)
         f.close()
