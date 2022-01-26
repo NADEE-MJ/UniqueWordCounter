36290468004a4681560c8ed96f93a4e94ceb8d19
NADEE-MJ <nadeem.maida@gmail.com>
2022-01-25 19:20:56 -0800

    added comments to main and uniqueWordCounter

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     """
     This file runs the logic in the UnqiueWordCounter Class, change the url variable to any url link that contains text data and it will run the class to determine all unique words in the text.
 
-    The following example uses the passage Romeo and Juliet, prints the number of unique words and 10 top ten occurances to the console and saves the entire formatted text and list of unique words and the number of times they each occur 
+    The following example uses the passage Romeo and Juliet, prints the number of unique words and 10 top ten occurances to the console and outputfile and saves the entire formatted text and list of unique words and the number of times they each occur 
     """
     url = "https://shakespeare.folger.edu/downloads/txt/romeo-and-juliet_TXT_FolgerShakespeare.txt"
     rAndJ = c.UniqueWordCounter(url)
--- uniqueWordCounter.py
+++ uniqueWordCounter.py
@@ -4,7 +4,11 @@ import json
 
 class UniqueWordCounter():
     """
-
+    given a url with text in it, this class can find the total number of unique words by building off of the counter module from collections, and output data on the text such as:
+    1.formatted text
+    2.list of all unique words ordered alphabetically
+    3.top ten most used unique words
+    4.number of unique words
     """
     def __init__(self, url):
         self.url = url
@@ -12,23 +16,55 @@ class UniqueWordCounter():
         self.__getEntireTextSplit()
 
     def __getURLData(self):
+        """
+        (self) -> (response object)
+
+        gets data from url
+        """
         return requests.get(self.url)
 
     def __getformattedText(self):
+        """
+        (self) -> (none)
+
+        formats text using regular expressions, uses this mask '[!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~]' to remove punctuation
+        """
         urlResponse = self.__getURLData()
         punctuationToRemove = '[' + string.punctuation.replace("'", "") + ']'
         self.formattedText = re.sub(punctuationToRemove, ' ', urlResponse.text)
 
     def __getEntireTextSplit(self):
+        """
+        (self) -> (none)
+
+        splits text into a list of all lowercase single words
+        """
         self.EntireTextSplit = self.formattedText.lower().split()
 
     def getUniqueWords(self):
+        """
+        (self) -> (none)
+
+        creates a counter object from the split text list which automatically counts the 
+        unqiue items in the list
+        """
         self.uniqueWords = Counter(self.EntireTextSplit)
 
     def getNumberOfUniqueWords(self):
+        """
+        (self) -> (str)
+
+        returns a string with the total number of unique words
+        """
         return "There are %d unqiue words in the provided text!\n" % (len(self.uniqueWords))
 
     def getTopTenWords(self):
+        """
+        (self) -> (str)
+
+        using counter class, finds the top 10 most common words and # of occurances
+        then adds them all to a string and returns it
+        """
         topTenString = "#. | Word | # of Occurances\n"
         topTen = self.uniqueWords.most_common(10)
         count = 1
@@ -39,23 +75,45 @@ class UniqueWordCounter():
         return topTenString
 
     def saveNumberOfUniquesAndTopTen(self, path):
+        """
+        (self, path) -> (none)
+
+        saves data from getNumberOfUniqueWords and getTopTenWords to a .txt file at
+        the given path destination
+        """
         f = open(path, 'w')
         f.write(self.getNumberOfUniqueWords() + "\n")
         f.write(self.getTopTenWords())
         f.close()
 
     def saveFormattedText(self, path):
+        """
+        (self, path) -> (none)
+
+        saves formatted text to a .txt file at the given path destination
+        """
         f = open(path, 'w')
         f.write(self.formattedText)
         f.close()
 
     def __getSortedUniqueWords(self):
+        """
+        (self) -> ({str : int})
+
+        sorts unique words into a dictionary in alphabetical order using sorted method
+        then returns the sorted dictionary
+        """
         sortedUniqueWords = {}
-        for word in sorted(self.uniqueWords):
+        for word in sorted(self.uniqueWords.keys()):
             sortedUniqueWords[word] = self.uniqueWords[word]
         return sortedUniqueWords
 
     def saveUniqueWords(self, path):
+        """
+        (self, path) -> (none)
+
+        saves SortedUniqueWords dictionary to a .json file at the given path destination
+        """
         f = open(path, 'w')
         json.dump(self.__getSortedUniqueWords(), f, indent = 4)
         f.close()
